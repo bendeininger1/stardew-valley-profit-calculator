2373a1a2ac2846ae5833462747e6b1aae7e9a419
bendeininger1 <57870564+bendeininger1@users.noreply.github.com>
2021-02-01 22:34:33 -0500

    Add server variable

--- app.py
+++ app.py
@@ -63,6 +63,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 #df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
 
 
