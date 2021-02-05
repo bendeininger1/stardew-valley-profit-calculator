8d4407d4dcc2b8fbe3c54dd3eeb96934b66e564f
bendeininger1 <57870564+bendeininger1@users.noreply.github.com>
2021-02-05 17:20:43 -0500

    Remove some silly seeds and added testing

--- data_import.py
+++ data_import.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 import json
 #Load crop data
 with open('Crops.json') as f:
@@ -15,17 +13,21 @@ crops 'Key' (Crop name): Days in each growing stage / Seasons spring, summer, fa
 Index of crop/regrow after harvest (-1 no regrowth, 3 = 3days),
 Harvest Method,Chance for extra harvest false or true
 '''
-def parsed_crops():
+def parsed_crops_function():
+    crops.pop('802')#remove Cactus Seeds
+    crops.pop('890')#remove QI Seeds
+    crops.pop('885')#remove Fiber Seeds
+    crops.pop('495')#remove Spring Seeds
+    crops.pop('496')#remove Summer Seeds
+    crops.pop('497')#remove Fall Seeds
+    crops.pop('498')#remove Winter Seeds
+    
     for i in crops:
         #split dict values into a list using the '/' serparator
-        #print(str(i)+str(crops[i]))
-        crops[i] = crops[i].split('/')
-        #split the 'days in each cycle' into a list, covnerts to int, then sum that list returing  total days to grow
+        crops[i] = str(crops[i]).split('/')#split the 'days in each cycle' into a list, covnerts to int, then sum that list returing  total days to grow
         crops[i][0] = sum([int (x) for x in crops[i][0].split(' ')])
         #split the 'Regrow after Harvest'
-        #print(crops[i][6])
         crops[i][6] = crops[i][6].split(' ')
-        #print(crops[i][6])
         #convert true/false to boolean of 'Regrow after Harvest'
         crops[i][6][0] = crops[i][6][0] == 'true'
         if (crops[i][6][0]== True):
@@ -33,30 +35,41 @@ def parsed_crops():
                 crops[i][6][x] = int(crops[i][6][x])
             crops[i][6][4] = float(crops[i][6][4])
         # the 'season' into a list
-        #print(crops[i][1])
         crops[i][1] = crops[i][1].split(' ')
-        #print(crops[i][1])
-        '''
-    ### maybe turn this into a data frame?
-    print('look here')
-    print(crops)
-    data={'Seed Index':,
-          }
-    crops_df=pd.DataFrame(data)
-    '''
+        
+        
     return(crops)
-crops=parsed_crops()
-#print(parsed_crops())
+
+parsed_crops=parsed_crops_function()
 
 
 '''parse the object information
 "493": "Cranberry Seeds/120/-300/Seeds -74/Cranberry Seeds/Plant these in the fall.
         Takes 7 days to mature, and continues to produce after first harvest."
 '''
-def parsed_object_information():
+def parsed_object_information_function():
     for i in ObjectInformation:
     #split dict values into a list using the '/' serparator
-        ObjectInformation[i] = ObjectInformation[i].split('/')
-        ObjectInformation[i]
+        ObjectInformation[i] = str(ObjectInformation[i]).split('/')
     return(ObjectInformation)
-ObjectInformation=parsed_object_information()
+parsed_object_information=parsed_object_information_function()
+
+if __name__ == '__main__':
+    import pandas as pd
+    x=parsed_crops
+    #print(x)
+    y=parsed_object_information
+    #print(y)
+    #print(ObjectInformation)
+    #print()
+    for i in parsed_crops:
+        print(str(i)+str(parsed_object_information[i][0]))
+        
+    print(x.get('472'))
+    print(y['472'][0])
+    #print(parsed_object_information()[parsed_crops['472'][3]][0])
+    
+    #x=crops
+    #print(x)
+    #parsed_crops()
+    
