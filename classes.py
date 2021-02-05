2002efb8974cb6f78a1d0dcba3571b64872e7014
bendeininger1 <57870564+bendeininger1@users.noreply.github.com>
2021-02-05 17:21:37 -0500

    Changed variable names from data_import

--- classes.py
+++ classes.py
@@ -13,16 +13,17 @@ class SeedSelection:
                  days_to_grow=None,
                  regrowth_days=None,
                  growing_seasons=None):
+        
         self.seed_number = int(seed_number)
         self.crop_count = crop_count 
-        self.seed_name = data_import.ObjectInformation[seed_number][0]
-        self.crop_name = data_import.ObjectInformation[data_import.crops[seed_number][3]][0]
+        self.seed_name = data_import.parsed_object_information[seed_number][0]
+        self.crop_name = data_import.parsed_object_information[data_import.parsed_crops[seed_number][3]][0]
         self.harvest_multiplier = multipliers.average_harvest_yield(seed_number)
-        self.seed_price = int(data_import.ObjectInformation[seed_number][1])
-        self.crop_price = int(data_import.ObjectInformation[data_import.crops[seed_number][3]][1])
-        self.days_to_grow = int(data_import.crops[seed_number][0])
-        self.regrowth_days = int(data_import.crops[seed_number][4])
-        self.growing_seasons =data_import.crops[seed_number][1] #growing_seasons #['spring']
+        self.seed_price = int(data_import.parsed_object_information[seed_number][1])
+        self.crop_price = int(data_import.parsed_object_information[data_import.parsed_crops[seed_number][3]][1])
+        self.days_to_grow = int(data_import.parsed_crops[seed_number][0])
+        self.regrowth_days = int(data_import.parsed_crops[seed_number][4])
+        self.growing_seasons =data_import.parsed_crops[seed_number][1] #growing_seasons #['spring']
         ### need to add seed1.quality_multiplier
 class Fertilizer:
     def __init__(self,
@@ -66,46 +67,15 @@ class Fertilizer:
                 raise Exception("Wrong Fertilizer Selected from fertilizer class")
         if purchase_fertilizer == False:
             self.cost = 0.0
-            print('poopy')
-
         
         
-        
-#runs only when testing and not on load
-#if __name__ == '__main__':
         
 
-'''
-seed1 = SeedSelection(data_import.parsed_object_information[seed1input[0]][0],
-                      ObjectInformation[crops[seed1input[0]][3]][0],
-                      seed1input[1],
-                      average_harvest_yield(seed1input[0]),
-                      ObjectInformation[seed1input[0]][1],
-                      ObjectInformation[crops[seed1input[0]][3]][1],
-                      crops[seed1input[0]][0],
-                      crops[seed1input[0]][4],
-                      crops[seed1input[0]][1])
-'''
-'''
-class SeedSelection:
-    def __init__(self,
-                 seed_name,
-                 crop_name,
-                 crop_count,
-                 harvest_multiplier,
-                 seed_price,
-                 crop_price,
-                 days_to_grow,
-                 regrowth_days,
-                 growing_seasons):
-        self.seed_name = seed_name
-        self.crop_name = crop_name
-        self.crop_count = crop_count
-        self.harvest_multiplier = harvest_multiplier
-        self.seed_price = seed_price
-        self.crop_price = crop_price
-        self.days_to_grow = days_to_grow
-        self.regrowth_days = regrowth_days
-        self.growing_seasons = growing_seasons
-        ### need to add seed1.quality_multiplier
-'''
+if __name__ == '__main__':
+    seeds=SeedSelection('472', 1).crop_count
+    print(seeds)
+    
+    #x=crops
+    #print(x)
+    #parsed_crops()
+    
